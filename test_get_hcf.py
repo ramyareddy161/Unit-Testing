db1c2ec8c34e30792df3015de33fe3f781272cef
Mouli <sreekar.mouli1998@gmail.com>
2018-03-18 22:08:14 +0530

    Removed unittest in test_get_hcf.py

--- test_get_hcf.py
+++ test_get_hcf.py
@@ -1,29 +1,24 @@
-import unittest
-from problem19 import get_hcf
+class TestGetHcf():
+    def __init__(self, get_hcf):
+        self.score = 0
+        self.get_hcf = get_hcf
 
-class TestGetHcf(unittest.TestCase):
-    score = 0
-    def tearDown(self):
-        print('Running Total: ', TestGetHcf.score)
     def test_gethcf_method_1(self):
-        self.assertEqual(get_hcf([], [(2, 5)]), [])
-        TestGetHcf.score += 2
+        if get_hcf([] == [(2, 5)]), []:
+        	self.score += 2
     
     def test_gethcf_method_2(self):
-        self.assertEqual(get_hcf([], []), [])
-        TestGetHcf.score += 3
+        if get_hcf([], []) == []:
+        	self.score += 3
 
     def test_gethcf_method_3(self):
-        self.assertEqual(get_hcf([(2, 3), (3, 3), (11, 1)], [(5, 2)]), [])
-        TestGetHcf.score += 5
+        if get_hcf([(2, 3), (3, 3), (11, 1)] == [(5, 2)]), []:
+        	self.score += 5
 
     def test_gethcf_method_4(self):
-        self.assertEqual(get_hcf([(2, 3), (5, 3), (17, 2)], [(2, 1), (3, 5), (5, 2), (19, 1)]), [(2, 1), (5, 2)])
-        TestGetHcf.score += 5
+        if get_hcf([(2, 3), (5, 3), (17, 2)], [(2, 1), (3, 5), (5, 2), (19, 1)]) == [(2, 1), (5, 2)]:
+        	self.score += 5
 
     def test_gethcf_method_5(self):
-        self.assertEqual(get_hcf([(2, 1), (11, 3), (19, 2), (7919, 4)], [(2, 2), (7, 1), (19, 1), (2687, 4)]), [(2, 1), (19, 1)])
-        TestGetHcf.score += 10
-
-if __name__ == '__main__':
-    unittest.main()
+        if get_hcf([(2, 1), (11, 3), (19, 2), (7919, 4)], [(2, 2), (7, 1), (19, 1), (2687, 4)]) == [(2, 1), (19, 1)]:
+        	self.score += 10
