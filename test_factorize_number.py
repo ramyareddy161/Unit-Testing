7ee596859ce9900930f8d68e7e4f628eeb6fb755
Mouli <sreekar.mouli1998@gmail.com>
2018-03-18 22:25:19 +0530

    Removed unittest in test_get_lcm.py. Fixed test_factorize_number.py, test_get_hcf.py

--- test_factorize_number.py
+++ test_factorize_number.py
@@ -4,25 +4,25 @@ class TestFactorizeNumber():
         self.factorize_number = factorize_number
 
     def test_method_1(self):
-        if factorize_number(1) == []:
+        if self.factorize_number(1) == []:
         	self.score += 2
 
     def test_method_2(self):
-        if factorize_number(6010) == [(2, 1), (5, 1), (601, 1)]:
+        if self.factorize_number(6010) == [(2, 1), (5, 1), (601, 1)]:
         	self.score += 5
 
     def test_method_3(self):
-        if factorize_number(7865228921869442) == [(2, 1), (7919, 4)]:
+        if self.factorize_number(7865228921869442) == [(2, 1), (7919, 4)]:
         	self.score += 10
 
     def test_method_4(self):
         try:
-            factorize_number(-10)
+            self.factorize_number(-10)
         except ValueError:
         	self.score += 3
 
     def test_method_5(self):
         try:
-            factorize_number(3.2)
+            self.factorize_number(3.2)
         except TypeError:
         	self.score += 5
--- test_get_hcf.py
+++ test_get_hcf.py
@@ -4,21 +4,21 @@ class TestGetHcf():
         self.get_hcf = get_hcf
 
     def test_gethcf_method_1(self):
-        if get_hcf([] == [(2, 5)]), []:
+        if self.get_hcf([] == [(2, 5)]), []:
         	self.score += 2
     
     def test_gethcf_method_2(self):
-        if get_hcf([], []) == []:
+        if self.get_hcf([], []) == []:
         	self.score += 3
 
     def test_gethcf_method_3(self):
-        if get_hcf([(2, 3), (3, 3), (11, 1)] == [(5, 2)]), []:
+        if self.get_hcf([(2, 3), (3, 3), (11, 1)] == [(5, 2)]), []:
         	self.score += 5
 
     def test_gethcf_method_4(self):
-        if get_hcf([(2, 3), (5, 3), (17, 2)], [(2, 1), (3, 5), (5, 2), (19, 1)]) == [(2, 1), (5, 2)]:
+        if self.get_hcf([(2, 3), (5, 3), (17, 2)], [(2, 1), (3, 5), (5, 2), (19, 1)]) == [(2, 1), (5, 2)]:
         	self.score += 5
 
     def test_gethcf_method_5(self):
-        if get_hcf([(2, 1), (11, 3), (19, 2), (7919, 4)], [(2, 2), (7, 1), (19, 1), (2687, 4)]) == [(2, 1), (19, 1)]:
+        if self.get_hcf([(2, 1), (11, 3), (19, 2), (7919, 4)], [(2, 2), (7, 1), (19, 1), (2687, 4)]) == [(2, 1), (19, 1)]:
         	self.score += 10
