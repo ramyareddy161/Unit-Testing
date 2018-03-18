959f6cba84ec84e479162b28d055bc1a4460d4d6
Mouli <sreekar.mouli1998@gmail.com>
2018-03-18 22:40:19 +0530

    Removed unittest in test_top_chars.py

--- test_top_chars.py
+++ test_top_chars.py
@@ -1,37 +1,34 @@
-import unittest
-from problem6 import top_chars
-
-class TestTopChars(unittest.TestCase):
-    score = 0
-    def tearDown(self):
-        print('Running Total: ', TestTopChars.score)
+class TestTopChars():
+    def __init__(self, top_chars):
+        self.score = 0
+        self.top_chars = top_chars
 
     def test_topchar_method_1(self):
-        with self.assertRaises(TypeError):
-            top_chars(None, 10)
-        TestTopChars.score += 1
+        try:
+            self.top_chars(None, 10)
+        except TypeError:
+        	self.score += 1
 
     def test_topchar_method_2(self):
-        with self.assertRaises(TypeError):
-            top_chars('hello', 'w')
-        TestTopChars.score += 1
+        try:
+            self.top_chars('hello', 'w')
+        except TypeError:
+        	self.score += 1
 
     def test_topchar_method_3(self):
-        with self.assertRaises(ValueError):
-            top_chars('hello', -1)
-        TestTopChars.score += 1
+        try:
+            self.top_chars('hello', -1)
+        except ValueError:
+        	self.score += 1
 
     def test_topchar_method_4(self):
-        self.assertEqual(top_chars('acceeeffff', 2), [('f', 4), ('e', 3)])
-        TestTopChars.score += 8
+        if self.top_chars('acceeeffff', 2) == [('f', 4), ('e', 3)]:
+        	self.score += 8
 
-    def test_topchar_method_4(self):
-        self.assertEqual(top_chars('ccggggeeff', 3), [('g', 4), ('f', 2), ('e', 2)])
-        TestTopChars.score += 8
+    def test_topchar_method_5(self):
+        if self.top_chars('ccggggeeff', 3) == [('g', 4), ('f', 2), ('e', 2)]:
+        	self.score += 8
     
-    def test_topchar_method_4(self):
-        self.assertEqual(top_chars('FFggggeeff', 10), [('g', 4), ('f', 2), ('e', 2), ('F', 2)])
-        TestTopChars.score += 6
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_topchar_method_6(self):
+        if self.top_chars('FFggggeeff', 10) == [('g', 4), ('f', 2), ('e', 2), ('F', 2)]:
+        	self.score += 6
