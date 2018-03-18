97cc1874ab12539393f1db2f7c71986c05530619
Mouli <sreekar.mouli1998@gmail.com>
2018-03-18 22:31:06 +0530

    Removed unittest in test_string_rotation.py

--- test_string_rotation.py
+++ test_string_rotation.py
@@ -1,30 +1,24 @@
-import unittest
-from problem1 import get_right_rotations
-
-class TestStringRotation(unittest.TestCase):
-    score = 0
-    def tearDown(self):
-        print('Running Total: ', TestStringRotation.score)
+class TestStringRotation():
+    def __init__(self, get_right_rotations):
+        self.score = 0
+        self.get_right_rotations = get_right_rotations
 
     def test_get_right_rotations_method_1(self):
-        self.assertEqual(get_right_rotations(None, None), -1)
-        TestStringRotation.score += 1
+        if self.get_right_rotations(None, None) == -1:
+        	self.score += 1
     
     def test_get_right_rotations_method_2(self):
-        self.assertEqual(get_right_rotations(None, 'abcd'), -1)
-        TestStringRotation.score += 1
+        if self.get_right_rotations(None, 'abcd') == -1:
+        	self.score += 1
 
     def test_get_right_rotations_method_3(self):
-        self.assertEqual(get_right_rotations('abcd', 'acbd'), -1)
-        TestStringRotation.score += 4
+        if self.get_right_rotations('abcd', 'acbd') == -1:
+        	self.score += 4
 
     def test_get_right_rotations_method_4(self):
-        self.assertEqual(get_right_rotations('abcd', 'cdab'), 2)
-        TestStringRotation.score += 5
+        if self.get_right_rotations('abcd', 'cdab') == 2:
+        	self.score += 5
     
     def test_get_right_rotations_method_5(self):
-        self.assertEqual(get_right_rotations('aeiou', 'aeiou'), 0)
-        TestStringRotation.score += 4
-
-if __name__ == '__main__':
-    unittest.main()
+        if self.get_right_rotations('aeiou', 'aeiou') == 0:
+        	self.score += 4
